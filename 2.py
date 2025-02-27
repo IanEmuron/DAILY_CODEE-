2415e7d4ce0c6829d6591f660fd5c75344e4f0d4
IAN EMURON <ianemuron03@gmail.com>
2025-02-27 17:08:50 +0300

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,20 +1,4 @@
-def calculator():
-    num1 = float(input("Enter first number: "))
-    op = input("Enter operator (+, -, *, /): ")
-    num2 = float(input("Enter second number: "))
-
-    if op == '+':
-        print(f"Result: {num1 + num2}")
-    elif op == '-':
-        print(f"Result: {num1 - num2}")
-    elif op == '*':
-        print(f"Result: {num1 * num2}")
-    elif op == '/':
-        if num2 != 0:
-            print(f"Result: {num1 / num2}")
-        else:
-            print("Cannot divide by zero!")
-    else:
-        print("Invalid operator!")
-
-calculator()
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print("Sum:", num1 + num2)
+print("Difference:", num1 - num2)
